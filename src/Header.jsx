a01d7452a030f80ee157ddd1bab3229b46a01346
absiddiq <siddiq.aiub@gmail.com>
2026-09-23 19:37:31 +0600

    add task section component and split its part into separate reusable component

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,13 +1,11 @@
 import taskerLogo from "./assets/tasker-logo.svg";
 export default function Header() {
   return (
-    <nav className="py-6 md:py-8 fixed top-0 w-full !bg-[#8a97b5] z-50">
+    <nav className="py-6 md:py-8 fixed top-0 w-full !bg-[#5e6c92] z-50">
       <div className="container mx-auto flex items-center justify-between gap-x-6">
-        {/* Logo  */}
         <a href="/">
-          <img className="h-[45px]" src={taskerLogo} alt="Lws" />
+          <img className="h-[45px]" src={taskerLogo} alt="Tasker" />
         </a>
-        {/* Logo Ends  */}
       </div>
     </nav>
   );
